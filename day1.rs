60e4355f67a33cc9e10e3052568b5747d756854f
Jared Nelson <d.jared.nelson@gmail.com>
2023-12-08 23:20:30 -0600

    day1, part2

--- day1.rs
+++ day1.rs
@@ -2,28 +2,46 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 
 fn main() {
-	let file = File::open("day1.txt")
-		.expect("day1.txt should be openable");
-	let lines = BufReader::new(file).lines();
-	let mut total = 0;
-	for line in lines {
-		let l = line
-			.expect("each line should be readable");
-		let calibration = first_digit(&l) * 10 + last_digit(&l);
-		//println!("{calibration}");
-		total += calibration;
-	}
-	println!("{total}");
+    let file = File::open("day1.txt").unwrap();
+    let lines = BufReader::new(file).lines();
+    let mut total1 = 0;
+    let mut total2 = 0;
+    for line in lines {
+        let l = line.unwrap();
+        total1 += calibration(&l, false);
+        total2 += calibration(&l, true);
+    }
+    println!("{total1}");
+    println!("{total2}");
 }
 
-fn first_digit(line: &str) -> u32 {
-	let d = line.chars().find(|c| c.is_digit(10))
-		.expect("line should have at least one digit");
-	d.to_digit(10)
-		.expect("found digit character should be a number")
+fn calibration(line: &str, allow_spelled: bool) -> u32 {
+    let mut first_digit: Option<u32> = None;
+    let mut last_digit: Option<u32> = None;
+    let mut cursor: &str = &line;
+    while !cursor.is_empty() {
+        let d = front_digit(&cursor, allow_spelled);
+        if d.is_some() {
+            if first_digit.is_none() { first_digit = d; }
+            last_digit = d;
+        }
+        cursor = &cursor[1..];
+    }
+    first_digit.unwrap() * 10 + last_digit.unwrap()
 }
 
-fn last_digit(line: &str) -> u32 {
-	let rev: String = line.chars().rev().collect();
-	first_digit(&rev)
+fn front_digit(cursor: &str, allow_spelled: bool) -> Option<u32> {
+    cursor.chars().next()?.to_digit(10).or_else(||
+        if !allow_spelled { None }
+        else if cursor.starts_with("one") { Some(1) }
+        else if cursor.starts_with("two") { Some(2) }
+        else if cursor.starts_with("three") { Some(3) }
+        else if cursor.starts_with("four") { Some(4) }
+        else if cursor.starts_with("five") { Some(5) }
+        else if cursor.starts_with("six") { Some(6) }
+        else if cursor.starts_with("seven") { Some(7) }
+        else if cursor.starts_with("eight") { Some(8) }
+        else if cursor.starts_with("nine") { Some(9) }
+        else { None }
+    )
 }
