b653bec9bee90d7ab79651eb49f465b82f3fbb5b
Jared Nelson <d.jared.nelson@gmail.com>
2023-12-13 14:58:56 -0600

    day12, part2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,66 +1,69 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn condition_report(springs: &Vec<bool>) -> Vec<u32> {
-    let mut report = Vec::new();
-    let mut run = 0;
-    for &spring in springs {
-        if spring {
-            run += 1;
-        } else if run != 0 {
-            report.push(run);
-            run = 0;
+fn can_match(springs: &[Option<bool>], run: usize) -> bool {
+    if springs.len() < run {
+        return false;
+    }
+    for i in 0..run {
+        if springs[i] == Some(false) {
+            return false;
         }
     }
-    if run != 0 {
-        report.push(run);
-    }
-    report
+    springs.len() == run || springs[run] != Some(true)
 }
 
-struct Arrangements<'a> {
-    springs: &'a Vec<Option<bool>>,
-    perm: u32,
-    end: u32,
-}
-
-impl Iterator for Arrangements<'_> {
-    type Item = Vec<bool>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.perm == self.end {
-            return None;
+fn arrangements(springs: &[Option<bool>], report: &[u32]) -> u64 {
+    thread_local!(static MEMO: RefCell<HashMap<(Vec<Option<bool>>, Vec<u32>), u64>> = RefCell::new(HashMap::new()));
+    MEMO.with(|memo| {
+        if let Some(v) = memo.borrow().get(&(springs.to_vec(), report.to_vec())) {
+            return *v;
         }
-        let mut a = Vec::new();
-        let mut mask = 1;
-        for s in self.springs {
-            match s {
-                None => {
-                    a.push(self.perm & mask != 0);
-                    mask <<= 1;
-                }
-                Some(b) => {
-                    a.push(*b);
-                }
+        if report.is_empty() {
+            if !springs.iter().any(|&s| s == Some(true)) {
+                return 1;
+            } else {
+                return 0;
             }
         }
-        self.perm += 1;
-        Some(a)
-    }
-}
-
-fn arrangements(springs: &Vec<Option<bool>>) -> Arrangements {
-    Arrangements {
-        springs,
-        perm: 0,
-        end: 1 << springs.iter().filter(|s| s.is_none()).count(),
-    }
+        if springs.is_empty() {
+            return 0;
+        }
+        let v = match springs[0] {
+            Some(false) => arrangements(&springs[1..], report),
+            Some(true) => {
+                let run = report[0] as usize;
+                if can_match(springs, run) {
+                    if springs.len() == run {
+                        arrangements(&[], &report[1..])
+                    } else {
+                        arrangements(&springs[run + 1..], &report[1..])
+                    }
+                } else {
+                    0
+                }
+            }
+            None => {
+                let mut springs1 = vec![Some(false)];
+                springs1.append(&mut springs[1..].to_vec());
+                let mut springs2 = vec![Some(true)];
+                springs2.append(&mut springs[1..].to_vec());
+                arrangements(&springs1, report) + arrangements(&springs2, report)
+            }
+        };
+        memo.borrow_mut()
+            .insert((springs.to_vec(), report.to_vec()), v);
+        v
+    })
 }
 
 fn main() {
     let file = File::open("day12.txt").unwrap();
     let lines = BufReader::new(file).lines();
-    let mut total = 0;
+    let mut total1 = 0;
+    let mut total2 = 0;
     for line in lines {
         let l = line.unwrap();
         let (springs, report) = l.split_once(' ').unwrap();
@@ -74,12 +77,12 @@ fn main() {
             })
             .collect();
         let report: Vec<u32> = report.split(',').map(|n| n.parse().unwrap()).collect();
-
-        for arr in arrangements(&springs) {
-            if condition_report(&arr) == report {
-                total += 1;
-            }
-        }
+        total1 += arrangements(&springs, &report);
+        let x: [&[Option<bool>]; 5] = [&springs, &springs, &springs, &springs, &springs];
+        let springs: Vec<Option<bool>> = x.join(&None);
+        let report = report.repeat(5);
+        total2 += arrangements(&springs, &report);
     }
-    println!("{total}");
+    println!("{total1}");
+    println!("{total2}");
 }
