977b8ec880d5b8ff2b5e6fed7b676ad16219d188
Jared Nelson <d.jared.nelson@gmail.com>
2023-12-12 21:16:52 -0600

    day9, part2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -22,13 +22,25 @@ fn extrapolate(vs: &Vec<i32>) -> i32 {
     }
 }
 
+fn extrapolate_back(vs: &Vec<i32>) -> i32 {
+    if is_zeroes(&vs) { 0 }
+    else {
+        let ds = delta(&vs);
+        let prev_d = extrapolate_back(&ds);
+        vs.first().unwrap() - prev_d
+    }
+}
+
 fn main() {
     let file = File::open("day9.txt").unwrap();
     let lines = BufReader::new(file).lines();
-    let mut sum = 0;
+    let mut sum_next = 0;
+    let mut sum_prev = 0;
     for line in lines {
         let values: Vec<i32> = line.unwrap().split(' ').map(|v| v.parse().unwrap()).collect();
-        sum += extrapolate(&values);
+        sum_next += extrapolate(&values);
+        sum_prev += extrapolate_back(&values);
     }
-    println!("{sum}");
+    println!("{sum_next}");
+    println!("{sum_prev}");
 }
