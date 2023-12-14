4dd430930acd1e5219e9266c577255d6d601401c
Jared Nelson <d.jared.nelson@gmail.com>
2023-12-13 23:05:34 -0600

    rustfmt

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -2,11 +2,13 @@ use std::cmp;
 use std::fs;
 
 fn row_mismatches<T: PartialEq>(g: &[T], width: usize, r1: usize, r2: usize) -> usize {
-    (0..width).filter(|c| g[r1*width+c] != g[r2*width+c]).count()
+    (0..width)
+        .filter(|c| g[r1 * width + c] != g[r2 * width + c])
+        .count()
 }
 
 fn test_h<T: PartialEq>(g: &[T], width: usize, before_row: usize) -> usize {
-    let num_rows = cmp::min(before_row, g.len()/width - before_row);
+    let num_rows = cmp::min(before_row, g.len() / width - before_row);
     assert!(num_rows != 0);
     let mut mismatches = 0;
     for i in 0..num_rows {
@@ -18,14 +20,18 @@ fn test_h<T: PartialEq>(g: &[T], width: usize, before_row: usize) -> usize {
 }
 
 fn try_h<T: PartialEq>(g: &[T], width: usize, mismatches: usize) -> Option<usize> {
-    for i in 1..g.len()/width {
-        if test_h(g, width, i) == mismatches { return Some(i) }
+    for i in 1..g.len() / width {
+        if test_h(g, width, i) == mismatches {
+            return Some(i);
+        }
     }
     None
 }
 
 fn col_mismatches<T: PartialEq>(g: &[T], width: usize, c1: usize, c2: usize) -> usize {
-    (0..(g.len()/width)).filter(|r| g[r*width+c1] != g[r*width+c2]).count()
+    (0..(g.len() / width))
+        .filter(|r| g[r * width + c1] != g[r * width + c2])
+        .count()
 }
 
 fn test_v<T: PartialEq>(g: &[T], width: usize, before_col: usize) -> usize {
@@ -42,7 +48,9 @@ fn test_v<T: PartialEq>(g: &[T], width: usize, before_col: usize) -> usize {
 
 fn try_v<T: PartialEq>(g: &[T], width: usize, mismatches: usize) -> Option<usize> {
     for i in 1..width {
-        if test_v(g, width, i) == mismatches { return Some(i) }
+        if test_v(g, width, i) == mismatches {
+            return Some(i);
+        }
     }
     None
 }
@@ -58,14 +66,22 @@ fn main() {
         let h = try_h(&g, width, 0);
         let v = try_v(&g, width, 0);
         assert!(h.is_some() ^ v.is_some());
-        if let Some(r) = h { total1 += 100 * r; }
-        if let Some(c) = v { total1 += c; }
+        if let Some(r) = h {
+            total1 += 100 * r;
+        }
+        if let Some(c) = v {
+            total1 += c;
+        }
 
         let h = try_h(&g, width, 1);
         let v = try_v(&g, width, 1);
         assert!(h.is_some() ^ v.is_some());
-        if let Some(r) = h { total2 += 100 * r; }
-        if let Some(c) = v { total2 += c; }
+        if let Some(r) = h {
+            total2 += 100 * r;
+        }
+        if let Some(c) = v {
+            total2 += c;
+        }
     }
     println!("{total1}");
     println!("{total2}");
