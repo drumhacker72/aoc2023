e6fa7e0e960b21f2625307c2b1763213ac239d6b
Jared Nelson <d.jared.nelson@gmail.com>
2023-12-18 00:31:07 -0600

    day18, part2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,15 +1,18 @@
-use nom::IResult;
 use nom::branch::alt;
-use nom::character::complete::{char, hex_digit1, u32};
+use nom::bytes::complete::take;
+use nom::character::complete::{char, i64};
 use nom::combinator::{map_res, value};
 use nom::sequence::{delimited, tuple};
-use std::collections::HashSet;
+use nom::IResult;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 #[derive(Clone, Copy, Debug)]
 enum Dir {
-    U, D, L, R,
+    U,
+    D,
+    L,
+    R,
 }
 
 fn parse_dir(s: &str) -> IResult<&str, Dir> {
@@ -21,27 +24,34 @@ fn parse_dir(s: &str) -> IResult<&str, Dir> {
     ))(s)
 }
 
-type Color = u32;
+type Color = (i64, Dir);
 
 fn parse_color(s: &str) -> IResult<&str, Color> {
-    map_res(
-        tuple((char('#'), hex_digit1)),
-        |(_, n)| u32::from_str_radix(n, 16)
-    )(s)
+    let (s, (_, n, d)) = tuple((
+        char('#'),
+        map_res(take(5usize), |n| i64::from_str_radix(n, 16)),
+        alt((
+            value(Dir::R, char('0')),
+            value(Dir::D, char('1')),
+            value(Dir::L, char('2')),
+            value(Dir::U, char('3')),
+        )),
+    ))(s)?;
+    Ok((s, (n, d)))
 }
 
-fn parse_line(s: &str) -> IResult<&str, (Dir, u32, Color)> {
+fn parse_line(s: &str) -> IResult<&str, (Dir, i64, Color)> {
     let (s, (d, _, n, _, c)) = tuple((
         parse_dir,
         char(' '),
-        u32,
+        i64,
         char(' '),
         delimited(char('('), parse_color, char(')')),
     ))(s)?;
     Ok((s, (d, n, c)))
 }
 
-fn in_dir(pos: (i32, i32), dir: Dir, run: i32) -> (i32, i32) {
+fn in_dir(pos: (i64, i64), dir: Dir, run: i64) -> (i64, i64) {
     match dir {
         Dir::U => (pos.0, pos.1 + run),
         Dir::D => (pos.0, pos.1 - run),
@@ -50,26 +60,37 @@ fn in_dir(pos: (i32, i32), dir: Dir, run: i32) -> (i32, i32) {
     }
 }
 
+fn run(insts: &[(Dir, i64)]) {
+    let mut cursor = (0, 0);
+    let mut shoelace = 0;
+    let mut edge = 0;
+    for &(d, n) in insts {
+        let last = cursor;
+        cursor = in_dir(cursor, d, n);
+        edge += n;
+        shoelace += last.0 * cursor.1 - last.1 * cursor.0;
+    }
+    // Shoelace formula gives the area from the middle of each cube, but we want
+    // all the way out to the edges. Cubes on the edge are easy, each one will
+    // contribute another 1/2 to the count. Cubes on the corner will either
+    // contribute 1/4 or 3/4 depending on whether it is inner or outer.
+    // I'm sure there exists some formal proof, but it's easy enough to imagine
+    // that inner and outer corners will balance out to leave only 4 outer corners
+    // unmatched, hence the extra 1 added on.
+    println!("{}", shoelace.abs() / 2 + edge / 2 + 1);
+}
+
 fn main() {
     let file = File::open("day18.txt").unwrap();
     let lines = BufReader::new(file).lines().map(|l| l.unwrap());
-    let mut cursor = (0, 0);
-    let mut last = (0, 0);
-    let mut shoelace = 0;
-    let mut edge = 0;
-    let mut seen = HashSet::new();
-    seen.insert((0, 0));
+    let mut insts1 = Vec::new();
+    let mut insts2 = Vec::new();
     for line in lines {
-        let (remaining, (d, n, c)) = parse_line(&line).unwrap();
+        let (remaining, (d1, n1, (n2, d2))) = parse_line(&line).unwrap();
         assert!(remaining == "");
-        last = cursor;
-        for _i in 0..n {
-            cursor = in_dir(cursor, d, 1);
-            seen.insert(cursor);
-            println!("{:?}", cursor);
-        }
-        shoelace += last.0 * cursor.1 - last.1 * cursor.0;
+        insts1.push((d1, n1));
+        insts2.push((d2, n2));
     }
-    //TODO: justify this
-    println!("{}", shoelace.abs() / 2 + seen.len() as i32 / 2 + 1);
+    run(&insts1);
+    run(&insts2);
 }
